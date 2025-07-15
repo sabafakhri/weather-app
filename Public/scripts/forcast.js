eff1baa1f8e4678e15428becb34dd1171bc80684
Saba Fakhri <sabafakhri71@gmail.com>
2025-07-15 20:02:46 +0300

    fix server issue

--- Public/scripts/forcast.js
+++ Public/scripts/forcast.js
@@ -1,26 +1,17 @@
-const key = "jDq7YVnezoKPplhXi6KexcA3kS1UqhmI";
-
 //get weather info
 const getWeather = async (id) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-    const query = `${id}?apikey=${key}`
-
-    const response = await fetch (base + query);
-    const data = await response.json();
     
+    const response = await fetch (`/api/weather/${id}`);
+    const data = await response.json();
     return data[0];
 
 }
 
 //get city info
 const getCity = async (city) => {
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const query = `?apikey=${key}&q=${city}`;
 
-
-    const response = await fetch (base + query);
+    const response = await fetch (`/api/city/${city}`);
     const data = await response.json();
-    
     return data[0];
 }
 
