b5324a09bb503ad9357597a8660d6fb32e2fce20
Saba Fakhri <sabafakhri71@gmail.com>
2025-07-15 14:31:14 +0300

    fix getCity getWeather and key

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,11 +1,14 @@
 const key = "jDq7YVnezoKPplhXi6KexcA3kS1UqhmI";
 
+//get weather info
 const getWeather = async (id) => {
     const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-    const query = `?${id}apikey=${key}`
+    const query = `${id}?apikey=${key}`
 
     const response = await fetch (base + query);
     const data = await response.json();
+    
+    return data[0];
 
 }
 
@@ -17,13 +20,13 @@ const getCity = async (city) => {
 
     const response = await fetch (base + query);
     const data = await response.json();
-
+    
     return data[0];
 }
 
-getCity("manchester").then(data => {
-    return getWeather(data.key);
-    }).then(data => {
-        console.log(data)
-    })
-    .catch(err => console.log(err))
+
+// getCity("espoo").then(data => {
+//     return getWeather(data.Key);
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(err => console.log(err))
