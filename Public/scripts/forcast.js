4775bc6acb6a9065c3e141981671e1b46cf114f5
Saba Fakhri <sabafakhri71@gmail.com>
2025-07-16 14:54:38 +0300

    deleted node & update js files

---
+++ Public/scripts/forcast.js
@@ -0,0 +1,34 @@
+const accuweatherKey = "GN7RGAQcs7GyEKJ9Ka0G1MKWPQTOwHMJ"
+
+const getWeather = async (id) => {
+    
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `?${id}apikey=${accuweatherKey}`
+
+    const response = await fetch (base + query);
+    const data = await response.json();
+    return data[0];
+    // console.log(data)
+
+}
+
+//get city info
+const getCity = async (city) => {
+
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${accuweatherKey}&q=${city}`;
+
+    const response = await fetch (base + query);
+    const data = await response.json();
+    return data[0];
+    // console.log(data)
+}
+
+
+getCity("espoo").then(data => {
+    // console.log(data.Key)
+    const id = (data.Key)
+    return getWeather(id);
+    }).then(data => {
+        console.log(data);
+    }).catch(err => console.log(err))
